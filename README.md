a607ca14740d69f8cc6ec6c53e2348b276ccac18
Olivia Johnson <olivia@o-j.co>
2021-04-04 15:22:23 -0500

    New repo for womens work covid project

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# womens-work-covid
+# womens-work
---
+++ scripts.js
@@ -0,0 +1,251 @@
+<script type="text/javascript">
+
+	$(document).scroll(function() {
+		var y = $(this).scrollTop();
+		if (y > 100) {
+			$('.arrow').fadeIn();
+		} else {
+			$('.arrow').fadeOut();
+		}
+	});
+
+
+	$(document).ready( function() {
+    $("#labels-1").hide(); //hide your div initially
+    var topOfOthDiv = $("#trigger-1").offset().top;
+    $(window).scroll(function() {
+        if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
+            $("#labels-1").fadeIn(); //reached the desired point -- show div
+        }
+        else{
+            $("#labels-1").fadeOut(); //else above the desired point -- hide div
+        }
+    });
+});
+
+	$(document).ready( function() {
+    $("#labels-2").hide(); //hide your div initially
+    var topOfOthDiv = $("#trigger-2").offset().top;
+    $(window).scroll(function() {
+        if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
+            $("#labels-2").fadeIn(); //reached the desired point -- show div
+        }
+        else{
+            $("#labels-2").fadeOut(); //else above the desired point -- hide div
+        }
+    });
+});
+
+		$(document).ready( function() {
+    $("#labels-2-m").hide(); //hide your div initially
+    var topOfOthDiv = $("#trigger-2").offset().top;
+    $(window).scroll(function() {
+        if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
+            $("#labels-2-m").fadeIn(); //reached the desired point -- show div
+        }
+        else{
+            $("#labels-2-m").fadeOut(); //else above the desired point -- hide div
+        }
+    });
+});
+
+	$(document).ready( function() {
+    $("#labels-3").hide(); //hide your div initially
+    var topOfOthDiv = $("#trigger-3").position().top;
+    $(window).scroll(function() {
+        if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
+            $("#labels-3").fadeIn(); //reached the desired point -- show div
+        }
+        else{
+            $("#labels-3").fadeOut(); //else above the desired point -- hide div
+        }
+    });
+});
+
+	$(document).ready( function() {
+    $("#labels-4").hide(); //hide your div initially
+    var topOfOthDiv = $("#trigger-4").position().top;
+    $(window).scroll(function() {
+        if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
+            $("#labels-4").fadeIn(); //reached the desired point -- show div
+        }
+        else{
+            $("#labels-4").fadeOut(); //else above the desired point -- hide div
+        }
+    });
+});
+	$(document).ready( function() {
+    $("#labels-4-m").hide(); //hide your div initially
+    var topOfOthDiv = $("#trigger-4").position().top;
+    $(window).scroll(function() {
+        if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
+            $("#labels-4-m").fadeIn(); //reached the desired point -- show div
+        }
+        else{
+            $("#labels-4-m").fadeOut(); //else above the desired point -- hide div
+        }
+    });
+});
+
+	$(document).ready( function() {
+    $("#labels-5").hide(); //hide your div initially
+    var topOfOthDiv = $("#trigger-5").position().top;
+    $(window).scroll(function() {
+        if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
+            $("#labels-5").fadeIn(); //reached the desired point -- show div
+        }
+        else{
+            $("#labels-5").fadeOut(); //else above the desired point -- hide div
+        }
+    });
+});
+
+	$(document).ready( function() {
+    $("#labels-6").hide(); //hide your div initially
+    var topOfOthDiv = $("#trigger-6").position().top;
+    $(window).scroll(function() {
+        if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
+            $("#labels-6").fadeIn(); //reached the desired point -- show div
+        }
+        else{
+            $("#labels-6").fadeOut(); //else above the desired point -- hide div
+        }
+    });
+});
+		$(document).ready( function() {
+    $("#labels-6-m").hide(); //hide your div initially
+    var topOfOthDiv = $("#trigger-6").position().top;
+    $(window).scroll(function() {
+        if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
+            $("#labels-6-m").fadeIn(); //reached the desired point -- show div
+        }
+        else{
+            $("#labels-6-m").fadeOut(); //else above the desired point -- hide div
+        }
+    });
+});
+
+	$(document).ready( function() {
+    $("#labels-7").hide(); //hide your div initially
+    var topOfOthDiv = $("#trigger-7").position().top;
+    $(window).scroll(function() {
+        if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
+            $("#labels-7").fadeIn(); //reached the desired point -- show div
+        }
+        else{
+            $("#labels-7").fadeOut(); //else above the desired point -- hide div
+        }
+    });
+});
+
+
+	$(document).ready( function() {
+    $("#labels-8").hide(); //hide your div initially
+    var topOfOthDiv = $("#trigger-8").position().top;
+    $(window).scroll(function() {
+        if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
+            $("#labels-8").fadeIn(); //reached the desired point -- show div
+        }
+        else{
+            $("#labels-8").fadeOut(); //else above the desired point -- hide div
+        }
+    });
+});
+
+	$(document).ready( function() {
+    $("#labels-8-m").hide(); //hide your div initially
+    var topOfOthDiv = $("#trigger-8").position().top;
+    $(window).scroll(function() {
+        if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
+            $("#labels-8-m").fadeIn(); //reached the desired point -- show div
+        }
+        else{
+            $("#labels-8-m").fadeOut(); //else above the desired point -- hide div
+        }
+    });
+});
+
+	$(document).ready( function() {
+    $("#labels-9").hide(); //hide your div initially
+    var topOfOthDiv = $("#trigger-9").position().top;
+    $(window).scroll(function() {
+        if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
+            $("#labels-9").fadeIn(); //reached the desired point -- show div
+        }
+        else{
+            $("#labels-9").fadeOut(); //else above the desired point -- hide div
+        }
+    });
+});
+
+
+
+	$(document).ready( function() {
+    $("#labels-10").hide(); //hide your div initially
+    var topOfOthDiv = $("#trigger-10").position().top;
+    $(window).scroll(function() {
+        if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
+            $("#labels-10").fadeIn(); //reached the desired point -- show div
+        }
+        else{
+            $("#labels-10").fadeOut(); //else above the desired point -- hide div
+        }
+    });
+});
+
+		$(document).ready( function() {
+    $("#labels-10-m").hide(); //hide your div initially
+    var topOfOthDiv = $("#trigger-10").position().top;
+    $(window).scroll(function() {
+        if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
+            $("#labels-10-m").fadeIn(); //reached the desired point -- show div
+        }
+        else{
+            $("#labels-10-m").fadeOut(); //else above the desired point -- hide div
+        }
+    });
+});
+
+	$(document).ready( function() {
+    $("#labels-11").hide(); //hide your div initially
+    var topOfOthDiv = $("#trigger-11").position().top;
+    $(window).scroll(function() {
+        if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
+            $("#labels-11").fadeIn(); //reached the desired point -- show div
+        }
+        else{
+            $("#labels-11").fadeOut(); //else above the desired point -- hide div
+        }
+    });
+});
+
+	$(document).ready( function() {
+    $("#labels-12").hide(); //hide your div initially
+    var topOfOthDiv = $("#trigger-12").position().top;
+    $(window).scroll(function() {
+        if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
+            $("#labels-12").fadeIn(); //reached the desired point -- show div
+        }
+        else{
+            $("#labels-12").fadeOut(); //else above the desired point -- hide div
+        }
+    });
+});
+
+		$(document).ready( function() {
+    $("#labels-12-m").hide(); //hide your div initially
+    var topOfOthDiv = $("#trigger-12").position().top;
+    $(window).scroll(function() {
+        if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
+            $("#labels-12-m").fadeIn(); //reached the desired point -- show div
+        }
+        else{
+            $("#labels-12-m").fadeOut(); //else above the desired point -- hide div
+        }
+    });
+});
+
+$(document).ready(function(){
+	$('#labels-1').zoom();
+});
+</script>
